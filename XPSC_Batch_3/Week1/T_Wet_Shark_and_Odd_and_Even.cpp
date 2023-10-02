52ab68e434674812dda46e10318442bd3909b351
Minhaj <minhajulislammuhammad@gmail.com>
2023-10-02 13:01:41 +0600

    week1

---
+++ XPSC_Batch_3/Week1/T_Wet_Shark_and_Odd_and_Even.cpp
@@ -0,0 +1,29 @@
+// Q: https://codeforces.com/problemset/problem/621/A
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    long long sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        sum += a[i];
+    }
+    sort(a, a + n);
+    if (sum % 2 == 0)
+        cout << sum;
+    else
+    {
+        for (int i = 0; i < n; i++)
+            if (a[i] % 2 == 1)
+            {
+                sum -= a[i];
+                break;
+            }
+        cout << sum;
+    }
+    return 0;
+}
---
+++ XPSC_Batch_3/Week1/U_Forgotten_Episode.cpp
@@ -0,0 +1,17 @@
+// Q: https://codeforces.com/problemset/problem/440/A
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long n;
+    cin >> n;
+    long long sum = 0;
+    for (int i = 1; i < n; i++)
+    {
+        long long x;
+        cin >> x;
+        sum += x;
+    }
+    cout << n * (n + 1) / 2 - sum;
+    return 0;
+}
